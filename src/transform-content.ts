bc8046955a0f18957a89cada5b3a4535557acf7a
Zhicheng WANG <zcwang@thoughtworks.com>
2019-10-16 14:06:48 +0800

    refactor: 抽取公共函数

--- src/transform-content.ts
+++ src/transform-content.ts
@@ -10,24 +10,33 @@ function createParameter(name: string): ts.ParameterDeclaration {
   return ts.createParameter([], [], undefined, name);
 }
 
-function createMethod(modifiers: ts.Modifier[], name: string, paramNames: string[]): ts.MethodDeclaration {
-  return ts.createMethod([], [], undefined, name, undefined, undefined,
-    paramNames.map(name => createParameter(name)), undefined, undefined);
+function modifierOf(entry: JsDocEntry) {
+  const modifiers: ts.Modifier[] = [];
+  if (entry.scope === 'static') {
+    modifiers.push(ts.createModifier(ts.SyntaxKind.StaticKeyword));
+  }
+  return modifiers;
+}
+
+function createMethod(entry: JsDocEntry): ts.MethodDeclaration {
+  return ts.createMethod([], modifierOf(entry), undefined, entry.name!, undefined, undefined,
+    entry.meta!.code!.paramnames!.map(name => createParameter(name)), undefined, undefined);
+}
+
+function createProperty(entry: JsDocEntry): ts.PropertyDeclaration {
+  return ts.createProperty([], modifierOf(entry), entry.name!, undefined, undefined, undefined);
 }
 
 export function transformContent(jsDocAst: JsDocEntry[]): ts.NodeArray<ts.Statement> {
-  // 找出所有顶级节点：顶级类，顶级函数
-  // 找出顶级类的所有属性和方法、静态属性静态和方法 prototype，this.属性 和 this.prototype.属性 都是实例属性，this.方法是静态方法，this.prototype.方法 是实例方法
-  // 找出所有属性
   const statements = getClasses(jsDocAst).map(clazz => {
     const variables = uniqBy(jsDocAst.filter(it => it.kind === 'member' && it.scope === 'instance' && it.memberof === clazz.name), 'name')
-      .map(it => ts.createProperty([], [], it.name!, undefined, undefined, undefined));
+      .map(it => createProperty(it));
     const methods = uniqBy(jsDocAst.filter(it => it.kind === 'function' && it.scope === 'instance' && it.memberof === clazz.name), 'name')
-      .map(it => createMethod([], it.name!, it.meta!.code!.paramnames!));
+      .map(it => createMethod(it));
     const staticVariables = uniqBy(jsDocAst.filter(it => it.kind === 'member' && it.scope === 'static' && it.memberof === clazz.name), 'name')
-      .map(it => ts.createProperty([], [ts.createModifier(ts.SyntaxKind.StaticKeyword)], it.name!, undefined, undefined, undefined));
+      .map(it => createProperty(it));
     const staticMethods = uniqBy(jsDocAst.filter(it => it.kind === 'function' && it.scope === 'static' && it.memberof === clazz.name), 'name')
-      .map(it => createMethod([ts.createModifier(ts.SyntaxKind.StaticKeyword)], it.name!, it.meta!.code!.paramnames!));
+      .map(it => createMethod(it));
     return ts.createClassDeclaration(
       [],
       [ts.createModifier(ts.SyntaxKind.DeclareKeyword)],
@@ -35,7 +44,7 @@ export function transformContent(jsDocAst: JsDocEntry[]): ts.NodeArray<ts.Statem
       [],
       [],
       [
-        createMethod([], 'constructor', clazz.meta!.code!.paramnames!),
+        ts.createConstructor([], [], clazz.meta!.code.paramnames!.map(name => createParameter(name)), undefined),
         ...variables,
         ...methods,
         ...staticVariables,
